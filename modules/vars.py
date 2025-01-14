f6b67c4cc06144952bf6696c02bebaa3bad65562
AJ PYTHON <anantschool741@gmail.com>
2025-01-14 17:49:42 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,5 +1,7 @@
-api_id = "21008992"
-api_hash = "da87f6dea5ed8cfe1a53617e33a35742"
-bot_token = "7744027925:AAHjy9HEASeJWITns1IylGzn2abHInM3WBs"
+import os
+
+API_ID    = os.environ.get("API_ID", "")
+API_HASH  = os.environ.get("API_HASH", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
 AUTH = "7548265642"
 #Bot Username = "MARCO_015"
