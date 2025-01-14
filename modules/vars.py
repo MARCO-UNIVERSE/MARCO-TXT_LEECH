69cbeb56311a6f62ba79dc2e8619c7234b871050
MARCO <194879232+MARCO-UNIVERSE@users.noreply.github.com>
2025-01-14 19:55:17 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,5 +1,7 @@
-api_id = "21008992"
-api_hash = "da87f6dea5ed8cfe1a53617e33a35742"
-bot_token = "7412398574:AAHCewLMBJ37kp_yxi-GZbUYuT_Kc1fG0oc"
+import os
+
+api_id    = os.environ.get("api_id", "")
+api_hash  = os.environ.get("api_hash", "")
+bot_token = os.environ.get("bot_token", "")
 AUTH = "7548265642"
 #Bot Username = "MARCO_015"
